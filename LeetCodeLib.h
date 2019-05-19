44d55c8ea6431952398055921d3ce4d925d087bc
Yuyang Huang <yuyangh@usc.edu>
2019-05-19 15:43:28 +0800

    add running environment

---
+++ LeetCodeLib.h
@@ -0,0 +1,21 @@
+
+
+#ifndef LEETCODE_LEETCODELIB_H
+#define LEETCODE_LEETCODELIB_H
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
+#endif //LEETCODE_LEETCODELIB_H
