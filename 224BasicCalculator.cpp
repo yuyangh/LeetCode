65cd6ffee79243c8ae671aa309a276554b01dee0
Yuyang Huang <yuyangh@usc.edu>
2019-05-23 09:59:59 +0800

    LeetCode 224 done

--- 224BasicCalculator.cpp
+++ 224BasicCalculator.cpp
@@ -1,65 +1,114 @@
 #include "LeetCodeLib.h"
 
 class Solution {
+
 public:
-	stack<int> reverse, order;
+	// iterative approach
 	int calculate(string s) {
 		int result = 0;
-		for (int i = 0; i < s.size(); ++i) {
-			if (s[i] == ' ') {
-				continue;
-			}
-			if (s[i] == ')') {
-				while (reverse.top() != '(') {
-					order.push(reverse.top());
-					reverse.pop();
+		int sign = 1;
+		stack<int> st;
+		for (int i = 0; i < s.size(); i++) {
+			// get the number from string
+			if (s[i] >= '0') {
+				int num = 0;
+				while (i < s.size() && s[i] >= '0') {
+					num *= 10;
+					num += int(s[i]) - '0';
+					i++;
 				}
-				reverse.pop();// get rid of "("
-				result = calculatePara();
-				reverse.push(result);
-			} else {
-				// check each input is a num or not
-				if (isdigit(s[i])) {
-					string numString = s.substr(i, 20);
-					int num = stoi(numString);
-					reverse.push(num);
-					i += to_string(num).size() - 1;
-				} else {
-					reverse.push((s[i]));
-				}
-			}
-		}
-		
-		// handle all nums in ()
-		if (reverse.empty()) {
-			return result;
-		} else {
-			while (!reverse.empty()) {
-				order.push(reverse.top());
-				reverse.pop();
-			}
-			return calculatePara();
-		}
-	}
-	
-	int calculatePara() {
-		int result = (order.top()), right;
-		int op;
-		order.pop();
-		// do operations on each num
-		while (!order.empty()) {
-			op = order.top();
-			order.pop();
-			right = order.top();
-			order.pop();
-			if (op == '+') {
-				result = result + right;
-			} else {
-				result = result - right;
+				i--;
+				result += num * sign;
+			} else if (s[i] == '+') {
+				sign = 1;
+			} else if (s[i] == '-') {
+				sign = -1;
+			} else if (s[i] == '(') {
+				st.push(result);
+				st.push(sign);
+				result = 0;
+				sign = 1;
+			} else if (s[i] == ')') {
+				sign = st.top();
+				st.pop();
+				result *= sign;
+				result += st.top();
+				st.pop();
 			}
 		}
 		return result;
 	}
+	
+	// prev own approach
+	
+	// #define LEFT_PARA_VALUE -999999 /* -999999 represents '('*/
+	// stack<int> reverse, order;
+	// int calculate(string s) {
+	// 	int result = 0;
+	//
+	// 	for (int i = 0; i < s.size(); ++i) {
+	// 		if (s[i] == ' ') {
+	// 			continue;
+	// 		}
+	// 		if(s[i]=='('){
+	// 			reverse.push(LEFT_PARA_VALUE);
+	// 			continue;
+	// 		}
+	// 		if (s[i] == ')') {
+	// 			while (reverse.top() != LEFT_PARA_VALUE/*represents '('*/) {
+	// 				order.push(reverse.top());
+	// 				reverse.pop();
+	// 			}
+	// 			reverse.pop();// get rid of "("
+	// 			result = calculatePara();
+	// 			reverse.push(result);
+	// 		} else {
+	// 			// check each input is a num or not
+	// 			if (isdigit(s[i])) {
+	// 				string numString = s.substr(i, 20);
+	// 				int num = stoi(numString);
+	// 				reverse.push(num);
+	// 				i += to_string(num).size() - 1;
+	// 			} else {
+	// 				reverse.push((s[i]));
+	// 			}
+	// 		}
+	// 	}
+	//
+	// 	// handle all nums in ()
+	// 	if (reverse.empty()) {
+	// 		return result;
+	// 	} else {
+	// 		while (!reverse.empty()) {
+	// 			order.push(reverse.top());
+	// 			reverse.pop();
+	// 		}
+	// 		return calculatePara();
+	// 	}
+	// }
+	//
+	// int calculatePara() {
+	// 	if(order.empty()){
+	// 		cout<<"Problem in order stack"<<endl;
+	// 		return 0;
+	// 	}
+	// 	int result = (order.top()), right;
+	// 	int op;
+	// 	order.pop();
+	// 	// do operations on each num
+	// 	while (!order.empty()) {
+	// 		op = order.top();
+	// 		order.pop();
+	// 		right = order.top();
+	// 		order.pop();
+	// 		if (op == '+') {
+	// 			result = result + right;
+	// 		} else {
+	// 			result = result - right;
+	// 		}
+	// 	}
+	// 	return result;
+	// }
 };
 
 string stringToString(string input) {
