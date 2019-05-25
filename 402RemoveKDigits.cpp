0be4370d64e1bc49bd1e5eb0f63046e0959e7531
Yuyang Huang <yuyangh@usc.edu>
2019-05-25 21:55:32 +0800

    LeetCode 402 has problem

--- 402RemoveKDigits.cpp
+++ 402RemoveKDigits.cpp
@@ -1,4 +1,5 @@
 #include "LeetCodeLib.h"
+
 /*
  * @lc app=leetcode id=402 lang=cpp
  *
@@ -7,36 +8,36 @@
 class Solution {
 public:
 	string removeKdigits(string num, int k) {
-		if(k>=num.size()){
+		if (k >= num.size()) {
 			return "0";
 		}
-		for(int i=0;i<num.size()-1;i++){
-			if(k==0){
+		for (int i = 0; i < num.size() - 1; i++) {
+			if (k == 0) {
 				break;
 			}
-			if(num[i]>num[i+1]){
-				num[i]='#';
+			if (num[i] > num[i + 1]) {
+				num[i] = '#';
 				i--;
 				k--;
 			}
 		}
 		
-		while(k>0){
+		while (k > 0) {
 			num.pop_back();
 			k--;
 		}
 		
-		num.erase(remove(num.begin(),num.end(),'#'),num.end());
+		num.erase(remove(num.begin(), num.end(), '#'), num.end());
 		
-		int zeroCount=0;
-		for (int i = 0; i < num.size()-1; ++i) {
-			if(num[i]!='0'){
+		int zeroCount = 0;
+		for (int i = 0; i < num.size() - 1; ++i) {
+			if (num[i] != '0') {
 				break;
-			}else{
+			} else {
 				zeroCount++;
 			}
 		}
-		num.erase(0,zeroCount);
+		num.erase(0, zeroCount);
 		return num;
 	}
 	
@@ -72,20 +73,37 @@ public:
 string stringToString(string input) {
 	assert(input.length() >= 2);
 	string result;
-	for (int i = 1; i < input.length() -1; i++) {
+	for (int i = 1; i < input.length() - 1; i++) {
 		char currentChar = input[i];
 		if (input[i] == '\\') {
-			char nextChar = input[i+1];
+			char nextChar = input[i + 1];
 			switch (nextChar) {
-				case '\"': result.push_back('\"'); break;
-				case '/' : result.push_back('/'); break;
-				case '\\': result.push_back('\\'); break;
-				case 'b' : result.push_back('\b'); break;
-				case 'f' : result.push_back('\f'); break;
-				case 'r' : result.push_back('\r'); break;
-				case 'n' : result.push_back('\n'); break;
-				case 't' : result.push_back('\t'); break;
-				default: break;
+				case '\"':
+					result.push_back('\"');
+					break;
+				case '/' :
+					result.push_back('/');
+					break;
+				case '\\':
+					result.push_back('\\');
+					break;
+				case 'b' :
+					result.push_back('\b');
+					break;
+				case 'f' :
+					result.push_back('\f');
+					break;
+				case 'r' :
+					result.push_back('\r');
+					break;
+				case 'n' :
+					result.push_back('\n');
+					break;
+				case 't' :
+					result.push_back('\t');
+					break;
+				default:
+					break;
 			}
 			i++;
 		} else {
