2e1a57ebc6a529e6cdcbbcbd922a5693ea9e2a4e
Yuyang Huang <yuyangh@usc.edu>
2019-06-05 14:41:15 +0800

    LeetCode 198 done

---
+++ 198HouseRobber.cpp
@@ -0,0 +1,100 @@
+#include "LeetCodeLib.h"
+
+/*
+ * @lc app=leetcode id=198 lang=cpp
+ *
+ * [198] House Robber
+ *
+ * https://leetcode.com/problems/house-robber/description/
+ *
+ * algorithms
+ * Easy (40.99%)
+ * Likes:    2507
+ * Dislikes: 76
+ * Total Accepted:    322.6K
+ * Total Submissions: 786.2K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * You are a professional robber planning to rob houses along a street. Each
+ * house has a certain amount of money stashed, the only constraint stopping
+ * you from robbing each of them is that adjacent houses have security system
+ * connected and it will automatically contact the police if two adjacent
+ * houses were broken into on the same night.
+ *
+ * Given a list of non-negative integers representing the amount of money of
+ * each house, determine the maximum amount of money you can rob tonight
+ * without alerting the police.
+ *
+ * Example 1:
+ * Input: [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money =
+ * 3).
+ * Total amount you can rob = 1 + 3 = 4.
+ *
+ * Example 2:
+ * Input: [2,7,9,3,1]
+ * Output: 12
+ * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house
+ * 5 (money = 1).
+ * Total amount you can rob = 2 + 9 + 1 = 12.
+ */
+class Solution {
+public:
+	int rob(vector<int> &nums) {
+		// base case
+		if (nums.empty()) {
+			return 0;
+		}
+		if (nums.size() < 3) {
+			return *max_element(nums.begin(), nums.end());
+		}
+		vector<int> money(nums.size(), 0);
+		money[0] = nums[0];
+		money[1] = nums[1];
+		for (int i = 2; i < money.size(); i++) {
+			money[i] = max(money[i - 2] + nums[i], money[i - 1]);
+		}
+		return money[nums.size() - 1];
+	}
+};
+
+void trimLeftTrailingSpaces(string &input) {
+	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+		return !isspace(ch);
+	}));
+}
+
+void trimRightTrailingSpaces(string &input) {
+	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+		return !isspace(ch);
+	}).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+	vector<int> output;
+	trimLeftTrailingSpaces(input);
+	trimRightTrailingSpaces(input);
+	input = input.substr(1, input.length() - 2);
+	stringstream ss;
+	ss.str(input);
+	string item;
+	char delim = ',';
+	while (getline(ss, item, delim)) {
+		output.push_back(stoi(item));
+	}
+	return output;
+}
+
+int main() {
+	string line;
+	while (getline(cin, line)) {
+		vector<int> nums = stringToIntegerVector(line);
+		
+		int ret = Solution().rob(nums);
+		
+		string out = to_string(ret);
+		cout << out << endl;
+	}
+	return 0;
+}
