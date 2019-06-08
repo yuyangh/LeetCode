8805eec1445350c86a2217ea90efbe7183388932
Yuyang Huang <yuyangh@usc.edu>
2019-06-08 13:33:28 +0800

    LeetCode 098 done

---
+++ 098ValidateBinarySearchTree.cpp
@@ -0,0 +1,160 @@
+#include "LeetCodeLib.h"
+/*
+ * @lc app=leetcode id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ *
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (25.70%)
+ * Likes:    1947
+ * Dislikes: 295
+ * Total Accepted:    409.3K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ *
+ * Assume a BST is defined as follows:
+ *
+ *
+ * The left subtree of a node contains only nodes with keys less than the
+ * node's key.
+ * The right subtree of a node contains only nodes with keys greater than the
+ * node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ *
+ * Input: [2,1,3]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ *
+ * Input: [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is 4.
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	bool isValidBST(TreeNode *root) {
+		return valid(root, LONG_MIN, LONG_MAX);
+	}
+	
+	bool valid(TreeNode *node, long long lower, long long upper) {
+		if (node == nullptr) {
+			return true;
+		}
+		// right node must be larger
+		// left node must be smaller
+		if (node->val <= lower || node->val >= upper) {
+			return false;
+		}
+		bool left = valid(node->left, lower, node->val);
+		bool right = valid(node->right, node->val, upper);
+		// valid iff left and right both valid
+		return left && right;
+	}
+};
+
+
+void trimLeftTrailingSpaces(string &input) {
+	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+		return !isspace(ch);
+	}));
+}
+
+void trimRightTrailingSpaces(string &input) {
+	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+		return !isspace(ch);
+	}).base(), input.end());
+}
+
+TreeNode *stringToTreeNode(string input) {
+	trimLeftTrailingSpaces(input);
+	trimRightTrailingSpaces(input);
+	input = input.substr(1, input.length() - 2);
+	if (!input.size()) {
+		return nullptr;
+	}
+	
+	string item;
+	stringstream ss;
+	ss.str(input);
+	
+	getline(ss, item, ',');
+	TreeNode *root = new TreeNode(stoi(item));
+	queue<TreeNode *> nodeQueue;
+	nodeQueue.push(root);
+	
+	while (true) {
+		TreeNode *node = nodeQueue.front();
+		nodeQueue.pop();
+		
+		if (!getline(ss, item, ',')) {
+			break;
+		}
+		
+		trimLeftTrailingSpaces(item);
+		if (item != "null") {
+			int leftNumber = stoi(item);
+			node->left = new TreeNode(leftNumber);
+			nodeQueue.push(node->left);
+		}
+		
+		if (!getline(ss, item, ',')) {
+			break;
+		}
+		
+		trimLeftTrailingSpaces(item);
+		if (item != "null") {
+			int rightNumber = stoi(item);
+			node->right = new TreeNode(rightNumber);
+			nodeQueue.push(node->right);
+		}
+	}
+	return root;
+}
+
+string boolToString(bool input) {
+	return input ? "True" : "False";
+}
+
+int main() {
+	string line;
+	while (getline(cin, line)) {
+		TreeNode *root = stringToTreeNode(line);
+		
+		bool ret = Solution().isValidBST(root);
+		
+		string out = boolToString(ret);
+		cout << out << endl;
+	}
+	return 0;
+}
