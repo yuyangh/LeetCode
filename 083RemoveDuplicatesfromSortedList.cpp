8f276564022ddce6d6baa27ef3dd8124088c3165
Yuyang Huang <yuyangh@usc.edu>
2019-06-21 20:47:51 +0800

    LeetCode 083 done

---
+++ 083RemoveDuplicatesfromSortedList.cpp
@@ -0,0 +1,134 @@
+#include "LeetCodeLib.h"
+/*
+ * @lc app=leetcode id=83 lang=cpp
+ *
+ * [83] Remove Duplicates from Sorted List
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (42.66%)
+ * Likes:    782
+ * Dislikes: 82
+ * Total Accepted:    334.5K
+ * Total Submissions: 782.5K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a sorted linked list, delete all duplicates such that each element
+ * appear only once.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 1->1->2
+ * Output: 1->2
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: 1->1->2->3->3
+ * Output: 1->2->3
+ *
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* deleteDuplicates(ListNode* head) {
+		if (head == nullptr) {
+			return head;
+		}
+		
+		ListNode *node = head, *nextNode;
+		while (node != nullptr) {
+			nextNode = node->next;
+			// check duplicate
+			if (nextNode != nullptr) {
+				if (node->val == nextNode->val) {
+					node->next=nextNode->next;
+					delete nextNode;
+					continue;
+				}
+			}
+			node=nextNode;
+		}
+		return head;
+	}
+};
+
+void trimLeftTrailingSpaces(string &input) {
+	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+		return !isspace(ch);
+	}));
+}
+
+void trimRightTrailingSpaces(string &input) {
+	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+		return !isspace(ch);
+	}).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+	vector<int> output;
+	trimLeftTrailingSpaces(input);
+	trimRightTrailingSpaces(input);
+	input = input.substr(1, input.length() - 2);
+	stringstream ss;
+	ss.str(input);
+	string item;
+	char delim = ',';
+	while (getline(ss, item, delim)) {
+		output.push_back(stoi(item));
+	}
+	return output;
+}
+
+ListNode* stringToListNode(string input) {
+	// Generate list from the input
+	vector<int> list = stringToIntegerVector(input);
+	
+	// Now convert that list into linked list
+	ListNode* dummyRoot = new ListNode(0);
+	ListNode* ptr = dummyRoot;
+	for(int item : list) {
+		ptr->next = new ListNode(item);
+		ptr = ptr->next;
+	}
+	ptr = dummyRoot->next;
+	delete dummyRoot;
+	return ptr;
+}
+
+string listNodeToString(ListNode* node) {
+	if (node == nullptr) {
+		return "[]";
+	}
+	
+	string result;
+	while (node) {
+		result += to_string(node->val) + ", ";
+		node = node->next;
+	}
+	return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+	string line;
+	while (getline(cin, line)) {
+		ListNode* head = stringToListNode(line);
+		
+		ListNode* ret = Solution().deleteDuplicates(head);
+		
+		string out = listNodeToString(ret);
+		cout << out << endl;
+	}
+	return 0;
+}
