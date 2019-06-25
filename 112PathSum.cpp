adfc27bcb924c6d26bbcb4c480145a038702c361
Yuyang Huang <yuyangh@usc.edu>
2019-06-25 20:08:47 +0800

    LeetCode 112 done

---
+++ 112PathSum.cpp
@@ -0,0 +1,153 @@
+#include "LeetCodeLib.h"
+/*
+ * @lc app=leetcode id=112 lang=cpp
+ *
+ * [112] Path Sum
+ *
+ * https://leetcode.com/problems/path-sum/description/
+ *
+ * algorithms
+ * Easy (37.86%)
+ * Likes:    967
+ * Dislikes: 314
+ * Total Accepted:    321.5K
+ * Total Submissions: 846.5K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,null,1]\n22'
+ *
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path
+ * such that adding up all the values along the path equals the given sum.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Given the below binary tree and sum = 22,
+ *
+ *
+ * ⁠     5
+ * ⁠    / \
+ * ⁠   4   8
+ * ⁠  /   / \
+ * ⁠ 11  13  4
+ * ⁠/  \      \
+ * 7    2      1
+ *
+ *
+ * return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	bool hasPathSum(TreeNode *root, int sum) {
+		return traversal(root, 0, sum);
+	}
+	
+	bool traversal(TreeNode *node, int accumulation, int target) {
+		// base case
+		if (node == nullptr) {
+			return false;
+		}
+		// as leaf node
+		if (node->left == nullptr && node->right == nullptr) {
+			return accumulation + node->val == target;
+		}
+		// internal nodes
+		bool left = traversal(node->left, accumulation + node->val, target);
+		if (left) {
+			return true;
+		}
+		bool right = traversal(node->right, accumulation + node->val, target);
+		return right;
+	}
+};
+
+
+void trimLeftTrailingSpaces(string &input) {
+	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+		return !isspace(ch);
+	}));
+}
+
+void trimRightTrailingSpaces(string &input) {
+	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+		return !isspace(ch);
+	}).base(), input.end());
+}
+
+TreeNode *stringToTreeNode(string input) {
+	trimLeftTrailingSpaces(input);
+	trimRightTrailingSpaces(input);
+	input = input.substr(1, input.length() - 2);
+	if (!input.size()) {
+		return nullptr;
+	}
+	
+	string item;
+	stringstream ss;
+	ss.str(input);
+	
+	getline(ss, item, ',');
+	TreeNode *root = new TreeNode(stoi(item));
+	queue<TreeNode *> nodeQueue;
+	nodeQueue.push(root);
+	
+	while (true) {
+		TreeNode *node = nodeQueue.front();
+		nodeQueue.pop();
+		
+		if (!getline(ss, item, ',')) {
+			break;
+		}
+		
+		trimLeftTrailingSpaces(item);
+		if (item != "null") {
+			int leftNumber = stoi(item);
+			node->left = new TreeNode(leftNumber);
+			nodeQueue.push(node->left);
+		}
+		
+		if (!getline(ss, item, ',')) {
+			break;
+		}
+		
+		trimLeftTrailingSpaces(item);
+		if (item != "null") {
+			int rightNumber = stoi(item);
+			node->right = new TreeNode(rightNumber);
+			nodeQueue.push(node->right);
+		}
+	}
+	return root;
+}
+
+int stringToInteger(string input) {
+	return stoi(input);
+}
+
+string boolToString(bool input) {
+	return input ? "True" : "False";
+}
+
+int main() {
+	string line;
+	while (getline(cin, line)) {
+		TreeNode *root = stringToTreeNode(line);
+		getline(cin, line);
+		int sum = stringToInteger(line);
+		
+		bool ret = Solution().hasPathSum(root, sum);
+		
+		string out = boolToString(ret);
+		cout << out << endl;
+	}
+	return 0;
+}
