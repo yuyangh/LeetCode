60accf8bc99c3f229ad7e74d487aa1be638bb7af
Yuyang Huang <yuyangh@usc.edu>
2019-06-27 20:21:50 +0800

    LeetCode 067 done

--- 067AddBinary.cpp
+++ 067AddBinary.cpp
@@ -20,14 +20,10 @@
  * The input strings are both non-empty and contains only characters 1 or 0.
  *
  * Example 1:
- *
- *
  * Input: a = "11", b = "1"
  * Output: "100"
  *
  * Example 2:
- *
- *
  * Input: a = "1010", b = "1011"
  * Output: "10101"
  *
