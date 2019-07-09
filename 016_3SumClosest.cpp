a9cb949be9e270c4303dbcdb643fb0e90b1c544f
Yuyang Huang <yuyangh@usc.edu>
2019-07-09 16:41:54 +0800

    LeetCode 016 done

--- 016_3SumClosest.cpp
+++ 016_3SumClosest.cpp
@@ -136,16 +136,48 @@ private:
 		return mid;
 	}
 };
+void trimLeftTrailingSpaces(string &input) {
+	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+		return !isspace(ch);
+	}));
+}
+
+void trimRightTrailingSpaces(string &input) {
+	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+		return !isspace(ch);
+	}).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+	vector<int> output;
+	trimLeftTrailingSpaces(input);
+	trimRightTrailingSpaces(input);
+	input = input.substr(1, input.length() - 2);
+	stringstream ss;
+	ss.str(input);
+	string item;
+	char delim = ',';
+	while (getline(ss, item, delim)) {
+		output.push_back(stoi(item));
+	}
+	return output;
+}
+
+int stringToInteger(string input) {
+	return stoi(input);
+}
 
 int main() {
-	vector<int> arr = {-1, 2, 1, -4};
-	int target = 1;
-	
-	arr = {1, 2, 4, 8, 16, 32, 64, 128};
-	target = 82;
-	
-	Solution solution;
-	auto result = solution.threeSumClosest(arr, target);
-	PrintSingleResult(result);
+	string line;
+	while (getline(cin, line)) {
+		vector<int> nums = stringToIntegerVector(line);
+		getline(cin, line);
+		int target = stringToInteger(line);
+		
+		int ret = Solution().threeSumClosest(nums, target);
+		
+		string out = to_string(ret);
+		cout << out << endl;
+	}
 	return 0;
 }
