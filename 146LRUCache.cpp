51c367a5ebdf32be3e10f9c68796ea5c1e97d175
Yuyang Huang <yuyangh@usc.edu>
2019-07-18 15:35:19 +0800

    LeetCode 146 done

---
+++ 146LRUCache.cpp
@@ -0,0 +1,139 @@
+#include "LeetCodeLib.h"
+
+/*
+ * @lc app=leetcode id=146 lang=cpp
+ *
+ * [146] LRU Cache
+ *
+ * https://leetcode.com/problems/lru-cache/description/
+ *
+ * algorithms
+ * Medium (26.18%)
+ * Likes:    3181
+ * Dislikes: 114
+ * Total Accepted:    318K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '["LRUCache","put","put","get","put","get","put","get","get","get"]\n' +
+  '[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]'
+ *
+ * Design and implement a data structure for Least Recently Used (LRU) cache.
+ * It should support the following operations: get and put.
+ *
+ * get(key) - Get the value (will always be positive) of the key if the key
+ * exists in the cache, otherwise return -1.
+ * put(key, value) - Set or insert the value if the key is not already present.
+ * When the cache reached its capacity, it should invalidate the least recently
+ * used item before inserting a new item.
+ *
+ * The cache is initialized with a positive capacity.
+ *
+ * Follow up:
+ * Could you do both operations in O(1) time complexity?
+ *
+ * Example:
+ *
+ *
+ * LRUCache cache = new LRUCache( 2 // capacity // );
+ *
+ * cache.put(1, 1);
+ * cache.put(2, 2);
+ * cache.get(1);       // returns 1
+ * cache.put(3, 3);    // evicts key 2
+ * cache.get(2);       // returns -1 (not found)
+ * cache.put(4, 4);    // evicts key 1
+ * cache.get(1);       // returns -1 (not found)
+ * cache.get(3);       // returns 3
+ * cache.get(4);       // returns 4
+ *
+ */
+class LRUCache {
+public:
+	/*
+	 * use a hashtable and double-linked list to implement
+	 */
+	LRUCache(int capacity) : _capacity(capacity) {
+		// reserve enough space to decrease number of rehash
+		_record.reserve(2 * capacity);
+	}
+	
+	int get(int key) {
+		auto it = _record.find(key);
+		if (it == _record.end()) {
+			return -1;
+		} else {
+			// if we find the key, update the key's place
+			_order.erase(it->second.second);
+			_order.emplace_front(key);
+			it->second.second=_order.begin();
+			
+			return it->second.first;
+		}
+	}
+	
+	void put(int key, int value) {
+		auto it = _record.find(key);
+		if (it != _record.end()) {
+			// if we find the key,
+			// update the key's place and value
+			it->second.first=value;
+			_order.erase(it->second.second);
+			_order.emplace_front(key);
+			it->second.second=_order.begin();
+		} else {
+			// remove the oldest element
+			if (_record.size() >= _capacity) {
+				int earlist_key = _order.back();
+				_record.erase(earlist_key);
+				_order.pop_back();
+			}
+			// add the element
+			_order.emplace_front(key);
+			_record.emplace(key, make_pair(value, _order.begin()));
+		}
+	}
+
+private:
+	unordered_map<int, pair<int, std::list<int>::iterator>> _record;
+	list<int> _order;
+	int _capacity;
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+ 
+int main(){
+	int result;
+	LRUCache cache =  LRUCache( 2 /* capacity */ );
+	
+	// cache.put(1, 1);
+	// cache.put(2, 2);
+	// result=cache.get(1);        // returns 1
+	// PrintSingleResult(result);
+	// cache.put(3, 3);            // evicts key 2
+	// result=cache.get(2);        // returns -1 (not found)
+	// PrintSingleResult(result);
+	// cache.put(4, 4);            // evicts key 1
+	// result=cache.get(1);        // returns -1 (not found)
+	// PrintSingleResult(result);
+	// result=cache.get(3);        // returns 3
+	// PrintSingleResult(result);
+	// result=cache.get(4);        // returns 4
+	// PrintSingleResult(result);
+	
+	cache =  LRUCache( 1 /* capacity */ );
+	cache.put(2, 1);
+	result=cache.get(2);        // returns 1
+	PrintSingleResult(result);
+	cache.put(3, 2);            // evicts key 2
+	result=cache.get(2);        // returns -1 (not found)
+	PrintSingleResult(result);           // evicts key 1
+	result=cache.get(3);        // returns -1 (not found)
+	PrintSingleResult(result);
+	
+	return 0;
+}
+
