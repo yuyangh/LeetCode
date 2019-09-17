44d30586a2f20343eb60d07c76cf2dd9654cce80
Yuyang Huang <yuyangh@usc.edu>
2019-09-16 20:53:52 -0700

    Review

--- 021MergeTwoSortedLists.cpp
+++ 021MergeTwoSortedLists.cpp
@@ -1,4 +1,5 @@
 #include "LeetCodeLib.h"
+
 /**
 * Definition for singly-linked list.
 * struct ListNode {
@@ -9,50 +10,72 @@
 */
 class Solution {
 public:
-	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-		ListNode* head=nullptr,*tail=head;
-		ListNode* l1ptr=l1,*l2ptr=l2;
-		if(l1!=nullptr&&l2==nullptr){
+	ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+		ListNode dummy(INT_MIN);
+		ListNode *tail = &dummy;
+		
+		// check either list reach the end
+		while (l1 && l2) {
+			if (l1->val < l2->val) {
+				tail->next = l1;
+				l1 = l1->next;
+			} else {
+				tail->next = l2;
+				l2 = l2->next;
+			}
+			tail = tail->next;
+		}
+		
+		// connect non-empty list at the tail
+		tail->next = l1 ? l1 : l2;
+		return dummy.next;
+	}
+	
+	// code written initially
+	ListNode *mergeTwoListsInitial(ListNode *l1, ListNode *l2) {
+		ListNode *head = nullptr, *tail = head;
+		ListNode *l1ptr = l1, *l2ptr = l2;
+		if (l1 != nullptr && l2 == nullptr) {
 			return l1;
 		}
-		if(l1==nullptr&&l2!=nullptr){
+		if (l1 == nullptr && l2 != nullptr) {
 			return l2;
 		}
-		if(l1==nullptr&&l2==nullptr){
+		if (l1 == nullptr && l2 == nullptr) {
 			return nullptr;
 		}
 		
 		// two pointers go through the list
-		while(l1ptr!=nullptr||l2ptr!=nullptr){
-			if(l1ptr!=nullptr&&l2ptr==nullptr){
-				tail->next=l1ptr;
+		while (l1ptr != nullptr || l2ptr != nullptr) {
+			if (l1ptr != nullptr && l2ptr == nullptr) {
+				tail->next = l1ptr;
 				break;
 			}
-			if(l1ptr==nullptr&&l2ptr!=nullptr){
-				tail->next=l2ptr;
+			if (l1ptr == nullptr && l2ptr != nullptr) {
+				tail->next = l2ptr;
 				break;
 			}
-			if(l1ptr->val<l2ptr->val){
+			if (l1ptr->val < l2ptr->val) {
 				// handle head and tail situations differently
-				if(!head){
-					head=l1ptr;
-					tail=head;
-				}else{
-					tail->next=l1ptr;
-					tail=l1ptr;
+				if (!head) {
+					head = l1ptr;
+					tail = head;
+				} else {
+					tail->next = l1ptr;
+					tail = l1ptr;
 				}
-				l1ptr=l1ptr->next;
+				l1ptr = l1ptr->next;
 				
-			}else{
-				if(!head){
-					head=l2ptr;
-					tail=head;
-				}else{
-					tail->next=l2ptr;
-					tail=l2ptr;
+			} else {
+				if (!head) {
+					head = l2ptr;
+					tail = head;
+				} else {
+					tail->next = l2ptr;
+					tail = l2ptr;
 					
 				}
-				l2ptr=l2ptr->next;
+				l2ptr = l2ptr->next;
 				
 			}
 		}
@@ -90,14 +113,14 @@ vector<int> stringToIntegerVector(string input) {
 	return output;
 }
 
-ListNode* stringToListNode(string input) {
+ListNode *stringToListNode(string input) {
 	// Generate list from the input
 	vector<int> list = stringToIntegerVector(input);
 	
 	// Now convert that list into linked list
-	ListNode* dummyRoot = new ListNode(0);
-	ListNode* ptr = dummyRoot;
-	for(int item : list) {
+	ListNode *dummyRoot = new ListNode(0);
+	ListNode *ptr = dummyRoot;
+	for (int item : list) {
 		ptr->next = new ListNode(item);
 		ptr = ptr->next;
 	}
@@ -106,7 +129,7 @@ ListNode* stringToListNode(string input) {
 	return ptr;
 }
 
-string listNodeToString(ListNode* node) {
+string listNodeToString(ListNode *node) {
 	if (node == nullptr) {
 		return "[]";
 	}
@@ -122,11 +145,11 @@ string listNodeToString(ListNode* node) {
 int main() {
 	string line;
 	while (getline(cin, line)) {
-		ListNode* l1 = stringToListNode(line);
+		ListNode *l1 = stringToListNode(line);
 		getline(cin, line);
-		ListNode* l2 = stringToListNode(line);
+		ListNode *l2 = stringToListNode(line);
 		
-		ListNode* ret = Solution().mergeTwoLists(l1, l2);
+		ListNode *ret = Solution().mergeTwoLists(l1, l2);
 		
 		string out = listNodeToString(ret);
 		cout << out << endl;
