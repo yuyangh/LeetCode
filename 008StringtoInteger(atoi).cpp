8d7443e549dbadb89aae2ff2129c8dd54780b723
Yuyang Huang <yuyangh@usc.edu>
2019-10-30 17:06:06 -0700

    Review

--- 008StringtoInteger(atoi).cpp
+++ 008StringtoInteger(atoi).cpp
@@ -76,6 +76,30 @@
  */
 class Solution {
 public:
+	// more succint version
+	int myAtoiRef(string str) {
+		long result = 0;
+		// indicate the sign
+		int indicator = 1;
+		
+		int i = str.find_first_not_of(' ');
+		// in case it is all spaces
+		if (i >= str.size()) { return 0; }
+		
+		if (str[i] == '-' || str[i] == '+') {
+			indicator = (str[i] == '-') ? -1 : 1;
+			i++;
+		}
+		while ('0' <= str[i] && str[i] <= '9') {
+			result = result * 10 + (str[i] - '0');
+			i++;
+			if (result * indicator >= INT_MAX) { return INT_MAX; }
+			if (result * indicator <= INT_MIN) { return INT_MIN; }
+		}
+		
+		return result * indicator;
+	}
+	
 	int myAtoi(string str) {
 		bool negative = false, numExist = false;
 		long long num = 0;
@@ -125,21 +149,6 @@ public:
 		return (int) (num);
 	}
 	
-	// more succint version
-	int myAtoiRef(string str) {
-		long result = 0;
-		int indicator = 1;// indicate the sign
-		int i = str.find_first_not_of(' ');
-		if (str[i] == '-' || str[i] == '+')
-			indicator = (str[i++] == '-') ? -1 : 1;
-		while ('0' <= str[i] && str[i] <= '9') {
-			result = result * 10 + (str[i++] - '0');
-			if (result * indicator >= INT_MAX) return INT_MAX;
-			if (result * indicator <= INT_MIN) return INT_MIN;
-		}
-		
-		return result * indicator;
-	}
 };
 
 string stringToString(string input) {
