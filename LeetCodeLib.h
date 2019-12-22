e76318be6ec489ccf8e8a3e80a9ef2f05df3b534
Yuyang Huang <yuyangh@usc.edu>
2019-12-22 16:48:11 +0800

    add Split

--- LeetCodeLib.h
+++ LeetCodeLib.h
@@ -125,6 +125,23 @@ void PrintVectorVector(vector<vector<T>> &arr) {
 	}
 }
 
+std::vector<std::string> Split(const std::string &str, std::string delim = ",") {
+	// const char delim = ',';
+	std::vector<std::string> retVal;
+	
+	size_t start = 0;
+	size_t delimLoc = str.find_first_of(delim, start);
+	while (delimLoc != std::string::npos) {
+		retVal.emplace_back(str.substr(start, delimLoc - start));
+		
+		start = delimLoc + 1;
+		delimLoc = str.find_first_of(delim, start);
+	}
+	
+	retVal.emplace_back(str.substr(start));
+	return retVal;
+}
+
 /*
  * calculate the maximum height of the tree
  */
