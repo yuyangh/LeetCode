8cd4ce8975b6be6b8690fe6e4826d23d993d74b3
Yuyang Huang <yuyangh@usc.edu>
2020-05-21 20:43:31 -0700

    904 done

--- 904FruitIntoBaskets.cpp
+++ 904FruitIntoBaskets.cpp
@@ -26,7 +26,6 @@
  * Move to the next tree to the right of the current tree.  If there is no tree
  * to the right, stop.
  *
- *
  * Note that you do not have any choice after the initial choice of starting
  * tree: you must perform step 1, then step 2, then back to step 1, then step
  * 2, and so on until you stop.
@@ -36,59 +35,68 @@
  *
  * What is the total amount of fruit you can collect with this procedure?
  *
- *
- *
  * Example 1:
- *
- *
  * Input: [1,2,1]
  * Output: 3
  * Explanation: We can collect [1,2,1].
  *
- *
- *
  * Example 2:
- *
- *
  * Input: [0,1,2,2]
  * Output: 3
  * Explanation: We can collect [1,2,2].
  * If we started at the first tree, we would only collect [0, 1].
  *
- *
- *
  * Example 3:
- *
- *
  * Input: [1,2,3,2,2]
  * Output: 4
  * Explanation: We can collect [2,3,2,2].
  * If we started at the first tree, we would only collect [1, 2].
  *
- *
- *
  * Example 4:
- *
- *
  * Input: [3,3,3,1,2,1,1,2,3,3,4]
  * Output: 5
  * Explanation: We can collect [1,2,1,1,2].
  * If we started at the first tree or the eighth tree, we would only collect 4
  * fruits.
  *
- *
  * Note:
- *
- *
  * 1 <= tree.length <= 40000
  * 0 <= tree[i] < tree.length
- *
- *
+ */
+
+/*
+ * Translation:
+ * Find out the longest length of subarrays with at most 2 different numbers
  */
 class Solution {
 public:
-	int totalFruit(vector<int>& tree) {
-	
+	/*
+	 * using slide window
+	 * increase the length of the window iff the new window is longer
+	 */
+	int totalFruit(vector<int> &tree) {
+		unordered_map<unsigned int, int> count;
+		unsigned int slide_start = 0, slide_end = 0;
+		for (; slide_end < tree.size(); ++slide_end) {
+			// increase the window length
+			count[tree[slide_end]]++;
+			if (count.size() > 2) {
+				count[tree[slide_start]]--;
+				if (count[tree[slide_start]] == 0) {
+					count.erase(tree[slide_start]);
+				}
+				// decrease the windows length
+				slide_start++;
+			}
+		}
+		return slide_end - slide_start;
 	}
 };
+
+int main() {
+	vector<int> arr = {3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4};
+	Solution solution;
+	assert(solution.totalFruit(arr) == 5);
+	Complete();
+}
 
