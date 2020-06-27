d188a6344264ec585ec010e733fd607f9cbcd538
Yuyang Huang <yuyangh@usc.edu>
2020-06-26 20:01:21 -0700

    reviewed

--- 129. Sum Root to Leaf Numbers.cpp
+++ 129. Sum Root to Leaf Numbers.cpp
@@ -59,10 +59,14 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+
 class Solution {
 public:
-	int sum;
-	
+	/*
+	 * Time complexity: O(n)
+	 * dfs
+	 */
 	int sumNumbers(TreeNode *root) {
 		sum = 0;
 		vector<int> result;
@@ -71,6 +75,9 @@ public:
 	}
 
 private:
+	
+	int sum;
+	
 	void traverse(vector<int> &result, TreeNode *node, int value) {
 		if (node == nullptr) {
 			return;
@@ -88,74 +95,3 @@ private:
 		traverse(result, node->right, value);
 	}
 };
-
-void trimLeftTrailingSpaces(string &input) {
-	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
-		return !isspace(ch);
-	}));
-}
-
-void trimRightTrailingSpaces(string &input) {
-	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
-		return !isspace(ch);
-	}).base(), input.end());
-}
-
-TreeNode *stringToTreeNode(string input) {
-	trimLeftTrailingSpaces(input);
-	trimRightTrailingSpaces(input);
-	input = input.substr(1, input.length() - 2);
-	if (!input.size()) {
-		return nullptr;
-	}
-	
-	string item;
-	stringstream ss;
-	ss.str(input);
-	
-	getline(ss, item, ',');
-	TreeNode *root = new TreeNode(stoi(item));
-	queue<TreeNode *> nodeQueue;
-	nodeQueue.push(root);
-	
-	while (true) {
-		TreeNode *node = nodeQueue.front();
-		nodeQueue.pop();
-		
-		if (!getline(ss, item, ',')) {
-			break;
-		}
-		
-		trimLeftTrailingSpaces(item);
-		if (item != "null") {
-			int leftNumber = stoi(item);
-			node->left = new TreeNode(leftNumber);
-			nodeQueue.push(node->left);
-		}
-		
-		if (!getline(ss, item, ',')) {
-			break;
-		}
-		
-		trimLeftTrailingSpaces(item);
-		if (item != "null") {
-			int rightNumber = stoi(item);
-			node->right = new TreeNode(rightNumber);
-			nodeQueue.push(node->right);
-		}
-	}
-	return root;
-}
-
-int main() {
-	string line;
-	while (getline(cin, line)) {
-		TreeNode *root = stringToTreeNode(line);
-		
-		int ret = Solution().sumNumbers(root);
-		
-		string out = to_string(ret);
-		cout << out << endl;
-	}
-	return 0;
-}
