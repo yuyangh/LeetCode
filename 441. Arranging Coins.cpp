f130dbdcb6b8a50f1095324ae04d3a07c1da131f
Yuyang Huang <yuyangh@usc.edu>
2020-07-01 17:20:19 -0800

    done

---
+++ 441. Arranging Coins.cpp
@@ -0,0 +1,71 @@
+//
+// Created by Yuyang Huang on 7/2/20.
+//
+
+#include "LeetCodeLib.h"
+
+/*
+ * @lc app=leetcode id=441 lang=cpp
+ *
+ * [441] Arranging Coins
+ *
+ * https://leetcode.com/problems/arranging-coins/description/
+ *
+ * algorithms
+ * Easy (38.80%)
+ * Likes:    572
+ * Dislikes: 682
+ * Total Accepted:    142.5K
+ * Total Submissions: 338.9K
+ * Testcase Example:  '5'
+ *
+ * You have a total of n coins that you want to form in a staircase shape,
+ * where every k-th row must have exactly k coins.
+ * ⁠
+ * Given n, find the total number of full staircase rows that can be formed.
+ *
+ * n is a non-negative integer and fits within the range of a 32-bit signed
+ * integer.
+ *
+ * Example 1:
+ *
+ * n = 5
+ *
+ * The coins can form the following rows:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤
+ *
+ * Because the 3rd row is incomplete, we return 2.
+ *
+ *
+ *
+ * Example 2:
+ *
+ * n = 8
+ *
+ * The coins can form the following rows:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤ ¤
+ * ¤ ¤
+ *
+ * Because the 4th row is incomplete, we return 3.
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+public:
+	/*
+	 * Time complexity: O(1)
+	 * Since (1 + row) * row / 2 <= n
+	 * So, (r + 0.5) ^ 2 <= 2n + 0.25
+	 */
+	int arrangeCoins(int n) {
+		return floor(sqrt(2 * static_cast<double>(n) + 0.25) - 0.5);
+	}
+};
+// @lc code=end
+
