1b863259fcbd1f2d2d65ada8a302e5bd16698b8c
Yuyang Huang <yuyangh@usc.edu>
2020-07-16 20:28:06 -0700

    reviewed

--- 67. Add Binary.cpp
+++ 67. Add Binary.cpp
@@ -30,6 +30,9 @@
  */
 class Solution {
 public:
+	/*
+	 * Time complexity: O(max(m,n))
+	 */
 	string addBinary(string a, string b) {
 		string result = "";
 		int carry = 0;
@@ -39,7 +42,7 @@ public:
 		
 		unsigned int length = max(a.size(), b.size());
 		for (int i = 0; i < length; i++) {
-			int numA = 0, numB = 0;
+			int numA, numB;
 			if (i < a.size()) {
 				numA = a[i] - '0';
 			} else {
@@ -54,11 +57,13 @@ public:
 			carry = (numA + numB + carry) / 2;
 			result += '0' + sum;
 		}
+		
 		// append last carry
 		if (carry) {
 			result += '1';
 		}
-		// string supports pop_back()
+		
+		// remove extra 0
 		for (int i = result.size() - 1; i > 0; i--) {
 			if (result[i] == '0') {
 				result.pop_back();
@@ -66,66 +71,9 @@ public:
 				break;
 			}
 		}
+		
 		std::reverse(result.begin(), result.end());
 		return result;
 	}
 };
 
-
-string stringToString(string input) {
-	assert(input.length() >= 2);
-	string result;
-	for (int i = 1; i < input.length() - 1; i++) {
-		char currentChar = input[i];
-		if (input[i] == '\\') {
-			char nextChar = input[i + 1];
-			switch (nextChar) {
-				case '\"':
-					result.push_back('\"');
-					break;
-				case '/' :
-					result.push_back('/');
-					break;
-				case '\\':
-					result.push_back('\\');
-					break;
-				case 'b' :
-					result.push_back('\b');
-					break;
-				case 'f' :
-					result.push_back('\f');
-					break;
-				case 'r' :
-					result.push_back('\r');
-					break;
-				case 'n' :
-					result.push_back('\n');
-					break;
-				case 't' :
-					result.push_back('\t');
-					break;
-				default:
-					break;
-			}
-			i++;
-		} else {
-			result.push_back(currentChar);
-		}
-	}
-	return result;
-}
-
-int main() {
-	string line;
-	while (getline(cin, line)) {
-		string a = stringToString(line);
-		getline(cin, line);
-		string b = stringToString(line);
-		
-		string ret = Solution().addBinary(a, b);
-		
-		string out = (ret);
-		cout << out << endl;
-	}
-	return 0;
-}
