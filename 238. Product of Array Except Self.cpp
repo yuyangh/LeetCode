85974edcf2aea69cbce71c05b93ed426902fc11c
Yuyang Huang <yuyangh@usc.edu>
2020-08-27 15:47:36 -0700

    reviewed

--- 238. Product of Array Except Self.cpp
+++ 238. Product of Array Except Self.cpp
@@ -5,6 +5,9 @@
 #include "LeetCodeLib.h"
 
 /*
+ * 238. Product of Array Except Self
+ * Medium
+ *
  * Given an array nums of n integers where n > 1,
  * return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
  *
