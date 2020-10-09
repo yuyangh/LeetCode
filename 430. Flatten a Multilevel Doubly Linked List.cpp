3ac3892828febf8d82a9e3e12fcd87d710a3d7fd
Yuyang Huang <yuyangh@usc.edu>
2020-10-09 11:05:39 -0700

    reviewed

--- 430. Flatten a Multilevel Doubly Linked List.cpp
+++ 430. Flatten a Multilevel Doubly Linked List.cpp
@@ -30,68 +30,45 @@
  *
  *
  * Example 1:
- *
- *
  * Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
  * Output: [1,2,3,7,8,11,12,9,10,4,5,6]
  * Explanation:
  *
  * The multilevel linked list in the input is as follows:
- *
- *
- *
  * After flattening the multilevel linked list it becomes:
  *
- *
- *
- *
  * Example 2:
- *
- *
  * Input: head = [1,2,null,3]
  * Output: [1,3,2]
  * Explanation:
  *
  * The input multilevel linked list is as follows:
  *
- * ⁠ 1---2---NULL
- * ⁠ |
- * ⁠ 3---NULL
+ *  1---2---NULL
+ *  |
+ *  3---NULL
  *
  *
  * Example 3:
- *
- *
  * Input: head = []
  * Output: []
+ * How multilevel linked list is represented in test case:
  *
- *
- *
- *
- * How multilevel linked list is represented in test case:
- *
- * We use the multilevel linked list from Example 1 above:
- *
- *
- * ⁠1---2---3---4---5---6--NULL
- * ⁠        |
- * ⁠        7---8---9---10--NULL
- * ⁠            |
- * ⁠            11--12--NULL
+ * We use the multilevel linked list from Example 1 above:
+ * 1---2---3---4---5---6--NULL
+ *         |
+ *         7---8---9---10--NULL
+ *             |
+ *             11--12--NULL
  *
  * The serialization of each level is as follows:
- *
- *
  * [1,2,3,4,5,6,null]
  * [7,8,9,10,null]
  * [11,12,null]
  *
- *
  * To serialize all levels together we will add nulls in each level to signify
  * no node connects to the upper node of the previous level. The serialization
  * becomes:
- *
- *
  * [1,2,3,4,5,6,null]
  * [null,null,7,8,9,10,null]
  * [null,11,12,null]
@@ -99,18 +76,12 @@
  *
  * Merging the serialization of each level and removing trailing nulls we
  * obtain:
- *
- *
  * [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
  *
  *
  * Constraints:
- *
- *
  * Number of Nodes will not exceed 1000.
  * 1 <= Node.val <= 10^5
- *
- *
  */
 
 // @lc code=start
@@ -125,14 +96,15 @@ public:
 };
 */
 
+/*
+ * Time complexity: O(n)
+ */
 class Solution {
 public:
-	/*
-	 * Time complexity: O(n)
-	 */
 	Node *flatten(Node *head) {
 		stack<Node *> nextNodes;
 		auto node = head;
+		
 		while (node != nullptr) {
 			if (node->child != nullptr) {
 				if (node->next != nullptr) {
