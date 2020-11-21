02dd79cfb52336c91342d865f16c3eaec80767fd
Yuyang Huang <yuyangh@usc.edu>
2020-11-21 10:16:21 +0800

    done

---
+++ 946. Validate Stack Sequences.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Yuyang Huang on 11/20/20.
+//
+
+#include "LeetCodeLib.h"
+
+/*
+ * 946. Validate Stack Sequences
+ * Medium
+ *
+ * Given two sequences pushed and popped with distinct values,
+ * return true if and only if this could have been the result of
+ * a sequence of push and pop operations on an initially empty stack.
+ *
+ * Example 1:
+ * Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+ * Output: true
+ * Explanation: We might do the following sequence:
+ * push(1), push(2), push(3), push(4), pop() -> 4,
+ * push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+ *
+ * Example 2:
+ * Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+ * Output: false
+ * Explanation: 1 cannot be popped before 2.
+ *
+ * Constraints:
+ * 0 <= pushed.length == popped.length <= 1000
+ * 0 <= pushed[i], popped[i] < 1000
+ * pushed is a permutation of popped.
+ * pushed and popped have distinct values.
+ */
+
+/*
+ * Explanation:
+ * If the stack has say, 2 at the top,
+ * then if we have to pop that value next, we must do it now.
+ * That's because any subsequent push will make the top of the stack different from 2,
+ * and we will never be able to pop again.
+ *
+ * Time complexity:O(n)
+ */
+class Solution {
+public:
+	bool validateStackSequences(vector<int> &pushed, vector<int> &popped) {
+		stack<int> nums;
+		int popIdx = 0;
+		
+		for (int & num : pushed) {
+			nums.push(num);
+			while (!nums.empty() && popIdx < popped.size() &&nums.top()==popped[popIdx]){
+				nums.pop();
+				popIdx++;
+			}
+		}
+		return nums.empty();
+	}
+};
