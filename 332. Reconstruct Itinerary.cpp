26d793081b9d7aeb7c5bb4c4ff034e3c335bc7ec
Yuyang Huang <yuyangh@usc.edu>
2021-09-16 17:20:19 -0800

    reviewed

--- 332. Reconstruct Itinerary.cpp
+++ 332. Reconstruct Itinerary.cpp
@@ -13,10 +13,6 @@
  *
  * algorithms
  * Medium (33.03%)
- * Likes:    1757
- * Dislikes: 960
- * Total Accepted:    151K
- * Total Submissions: 421.5K
  * Testcase Example:  '[["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]'
  *
  * Given a list of airline tickets represented by pairs of departure and
@@ -25,8 +21,6 @@
  * with JFK.
  *
  * Note:
- *
- *
  * If there are multiple valid itineraries, you should return the itinerary
  * that has the smallest lexical order when read as a single string. For
  * example, the itinerary ["JFK", "LGA"] has a smaller lexical order than
@@ -37,26 +31,30 @@
  *
  *
  * Example 1:
- *
- *
  * Input: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
  * Output: ["JFK", "MUC", "LHR", "SFO", "SJC"]
  *
  *
  * Example 2:
- *
- *
  * Input:
  * [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
  * Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
  * Explanation: Another possible reconstruction is
  * ["JFK","SFO","ATL","JFK","ATL","SFO"].
  * But it is larger in lexical order.
- *
- *
  */
 
 // @lc code=start
+/*
+ * Explanation:
+ * First keep going forward until you get stuck. That's a good main path already.
+ * Remaining tickets form cycles which are found on the way back and get merged into that main path.
+ * By writing down the path backwards when retreating from recursion,
+ * merging the cycles into the main path is easy - the end part of the path has already been written,
+ * the start part of the path hasn't been written yet,
+ * so just write down the cycle now and then keep backwards-writing the path.
+ * link: https://leetcode.com/problems/reconstruct-itinerary/discuss/78768/Short-Ruby-Python-Java-C%2B%2B
+ */
 class Solution {
 public:
 	/*
