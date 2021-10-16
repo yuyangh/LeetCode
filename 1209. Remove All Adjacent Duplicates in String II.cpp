a2449501e3af5daabbb6975c3bf22ace60f84167
Yuyang Huang <yuyangh@usc.edu>
2021-10-15 17:20:19 -0800

    reviewed

--- 1209. Remove All Adjacent Duplicates in String II.cpp
+++ 1209. Remove All Adjacent Duplicates in String II.cpp
@@ -5,28 +5,19 @@
 #include "LeetCodeLib.h"
 
 /*
- * @lc app=leetcode id=1209 lang=cpp
- *
- * [1209] Remove All Adjacent Duplicates in String II
+ * 1209. Remove All Adjacent Duplicates in String II
  *
  * https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/description/
  *
  * algorithms
- * Medium (57.82%)
- * Likes:    748
- * Dislikes: 17
- * Total Accepted:    44.5K
- * Total Submissions: 77.8K
- * Testcase Example:  '"abcd"\n2'
+ * Medium
  *
  * Given a string s, a k duplicate removal consists of choosing k adjacent and
  * equal letters from s and removing them causing the left and the right side
  * of the deleted substring to concatenate together.
  *
  * We repeatedly make k duplicate removals on s until we no longer can.
- *
  * Return the final string after all such duplicate removals have been made.
- *
  * It is guaranteed that the answer is unique.
  *
  *
@@ -47,32 +38,29 @@
  * Input: s = "pbbcggttciiippooaais", k = 2
  * Output: "ps"
  *
- *
- *
  * Constraints:
  * 1 <= s.length <= 10^5
  * 2 <= k <= 10^4
  * s only contains lower case English letters.
- *
- *
  */
 
-// @lc code=start
-/*
- * Time complexity: O(n)
- */
 class Solution {
 public:
+	/*
+	 * Time complexity: O(n)
+	 * remove fixed amount of character,
+	 * use vector of char count to eliminate twice
+	 */
 	string removeDuplicates(string s, int k) {
 		vector<pair<char, int>> charCountArr;
 		string result;
 		
-		for (const auto &c : s) {
+		for (const auto &c: s) {
 			processChar(charCountArr, c, k);
 		}
 		
 		// reconstruct the string
-		for (const auto &item : charCountArr) {
+		for (const auto &item: charCountArr) {
 			if (item.second >= k) {
 				continue;
 			}
