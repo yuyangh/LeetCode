5d13b3664e818a509b2087f9e815da8880949e37
Yuyang Huang <yuyangh@usc.edu>
2021-11-02 17:20:19 -0800

    reviewed

--- 449. Serialize and Deserialize BST.cpp
+++ 449. Serialize and Deserialize BST.cpp
@@ -1,20 +1,13 @@
 #include "LeetCodeLib.h"
 /*
- * @lc app=leetcode id=449 lang=cpp
- *
- * [449] Serialize and Deserialize BST
+ * 449. Serialize and Deserialize BST
  *
  * https://leetcode.com/problems/serialize-and-deserialize-bst/description/
  *
  * algorithms
- * Medium (46.99%)
- * Likes:    611
- * Dislikes: 46
- * Total Accepted:    58.2K
- * Total Submissions: 123.5K
- * Testcase Example:  '[2,1,3]'
+ * Medium
  *
- * Serialization is the process of converting a data structure or object into a
+ * Serialization is the process of converting a binaryIndexTree structure or object into a
  * sequence of bits so that it can be stored in a file or memory buffer, or
  * transmitted across a network connection link to be reconstructed later in
  * the same or another computer environment.
@@ -58,7 +51,7 @@ public:
 		return str;
 	}
 	
-	// Decodes your encoded data to tree.
+	// Decodes your encoded binaryIndexTree to tree.
 	TreeNode *deserialize(string data) {
 		if (data.empty()) {
 			return nullptr;
@@ -115,98 +108,3 @@ public:
 // Codec codec;
 // codec.deserialize(codec.serialize(root));
 
-
-void trimLeftTrailingSpaces(string &input) {
-	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
-		return !isspace(ch);
-	}));
-}
-
-void trimRightTrailingSpaces(string &input) {
-	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
-		return !isspace(ch);
-	}).base(), input.end());
-}
-
-TreeNode *stringToTreeNode(string input) {
-	trimLeftTrailingSpaces(input);
-	trimRightTrailingSpaces(input);
-	input = input.substr(1, input.length() - 2);
-	if (!input.size()) {
-		return nullptr;
-	}
-	
-	string item;
-	stringstream ss;
-	ss.str(input);
-	
-	getline(ss, item, ',');
-	TreeNode *root = new TreeNode(stoi(item));
-	queue<TreeNode *> nodeQueue;
-	nodeQueue.push(root);
-	
-	while (true) {
-		TreeNode *node = nodeQueue.front();
-		nodeQueue.pop();
-		
-		if (!getline(ss, item, ',')) {
-			break;
-		}
-		
-		trimLeftTrailingSpaces(item);
-		if (item != "null") {
-			int leftNumber = stoi(item);
-			node->left = new TreeNode(leftNumber);
-			nodeQueue.push(node->left);
-		}
-		
-		if (!getline(ss, item, ',')) {
-			break;
-		}
-		
-		trimLeftTrailingSpaces(item);
-		if (item != "null") {
-			int rightNumber = stoi(item);
-			node->right = new TreeNode(rightNumber);
-			nodeQueue.push(node->right);
-		}
-	}
-	return root;
-}
-
-string treeNodeToString(TreeNode *root) {
-	if (root == nullptr) {
-		return "[]";
-	}
-	
-	string output = "";
-	queue<TreeNode *> q;
-	q.push(root);
-	while (!q.empty()) {
-		TreeNode *node = q.front();
-		q.pop();
-		
-		if (node == nullptr) {
-			output += "null, ";
-			continue;
-		}
-		
-		output += to_string(node->val) + ", ";
-		q.push(node->left);
-		q.push(node->right);
-	}
-	return "[" + output.substr(0, output.length() - 2) + "]";
-}
-
-int main() {
-	string line;
-	while (getline(cin, line)) {
-		TreeNode *root = stringToTreeNode(line);
-		Codec codec;
-		TreeNode *ret = codec.deserialize(codec.serialize(root));
-		
-		string out = treeNodeToString(ret);
-		cout << out << endl;
-	}
-	return 0;
-}
